eea152f6d495a48e675274a0d3c9691fe7c18f46
edp1096 <edp1096@naver.com>
2023-06-18 00:52:32 +0900

    Add example

--- examples/auto_tune/demo_auto_tune.py
+++ examples/auto_tune/demo_auto_tune.py
@@ -98,7 +98,9 @@ keys_gpu = [
 
 
 keys_to_test = unlimited_mem_keys
-use_gpu = False
+# use_gpu = False
+# keys_to_test = keys_gpu
+use_gpu = True
 
 
 if use_gpu:
--- examples/hello_faiss/main.py
+++ examples/hello_faiss/main.py
@@ -1,34 +1,40 @@
-""" 
-https://github.com/facebookresearch/faiss/wiki/Getting-started
-"""
-
+import faiss
 import numpy as np
 
-""" Getting some data """
-d = 64  # dimension
-nb = 100000  # database size
-nq = 10000  # nb of queries
-np.random.seed(1234)  # make reproducible
-xb = np.random.random((nb, d)).astype("float32")
-xb[:, 0] += np.arange(nb) / 1000.0
-xq = np.random.random((nq, d)).astype("float32")
-xq[:, 0] += np.arange(nq) / 1000.0
+
+""" Save index """
+items = np.array([
+    [1.2, 3.4, 5.6],
+    [2.1, 4.3, 6.5],
+    [0.8, 1.5, 2.7],
+    [3.2, 2.8, 1.4],
+], dtype=np.float32)
+
+# dimension = 3
+dimension = items[0].shape[0]
+index = faiss.IndexFlatL2(dimension)
+index.add(items)
+
+faiss.write_index(index, "index_euc.fss") # save index
+del index # unload index
 
 
-""" Building an index and adding the vectors to it """
-import faiss  # make faiss available
+""" Load index """
+index = faiss.read_index("index_euc.fss") # load index
 
-index = faiss.IndexFlatL2(d)  # build the index
-print(index.is_trained)
-index.add(xb)  # add vectors to the index
-print(index.ntotal)
+query = np.array([0.9, 2.3, 4.5], dtype=np.float32)
+
+n_neighbors = 2 # k
+dists_array, idxes_array = index.search(query.reshape(1, -1), n_neighbors)
+idxes, dists = idxes_array[0], dists_array[0]
+
+for idx, dist in zip(idxes, dists):
+    print(f"idx: {idx}, item: {items[idx]}, dist: {dist}")
 
 
-""" Searching """
-k = 4  # we want to see 4 nearest neighbors
-D, I = index.search(xb[:5], k)  # sanity check
-print(I)
-print(D)
-D, I = index.search(xq, k)  # actual search
-print(I[:5])  # neighbors of the 5 first queries
-print(I[-5:])  # neighbors of the 5 last queries
+""" Get distance between 1st and 2nd """
+index2 = faiss.IndexFlatL2(dimension)
+index2.add(items[0].reshape(1, -1))
+
+dists, _ = index2.search(items[2].reshape(1, -1), 1)
+print(f"distance between items[0] and items[2]: {dists[0][0]}")
